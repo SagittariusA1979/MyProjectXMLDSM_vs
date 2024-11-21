810cac98dc61652cee79d4ba9f2e088b0e940640
SagittariusA1979 <sagittariusa415@gmail.com>
2024-11-21 15:01:18 +0100

    Start 21.11.2014

---
+++ ModelViews/NavigatorModelView.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyProjectXMLDSM_vs.ModelViews;
+using MyProjectXMLDSM_vs.Views;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+
+namespace MyProjectXMLDSM_vs.ModelViews
+{
+    public partial class NavigatorModelView : ObservableObject
+    {
+        #region Instance
+        private static NavigatorModelView _instanceNav;
+        public static NavigatorModelView InstanceNav => _instanceNav ??= new NavigatorModelView();
+        #endregion
+
+        public NavigatorModelView() 
+        {
+            SelectViewModel = new StartView();
+        }
+
+        #region ObservalProperty
+        [ObservableProperty]
+        private object selectViewModel;
+
+        #endregion
+
+        #region RealyCommand
+        [RelayCommand]
+        private void ICitem01() 
+        {
+            SelectViewModel = new PrintView();
+        }
+
+        [RelayCommand]
+        private void ICitem02()
+        {
+            SelectViewModel = new PreparingView();
+        }
+
+        [RelayCommand] 
+        private void ICitem03()
+        {
+            SelectViewModel = new SettingView();
+        }
+        #endregion
+
+
+    }
+}
